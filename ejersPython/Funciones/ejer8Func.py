807f99e77c858e3e65b4ae334d623bfe9d107c04
manugra6 <156940273+manugra6@users.noreply.github.com>
2024-04-22 23:10:35 +0200

    Delete ejersPython directory

--- ejersPython/Funciones/ejer8Func.py
+++
@@ -1,9 +0,0 @@
-def factorial (num):
-    if num==0 or num==1:
-        return 1
-    else:
-        return num * factorial(num-1)
-    
-num=int(input("Introduce un numero para el calculo de su factorial: "))
-resultado = factorial(num)
-print(f"El factorial de {num} es: {resultado}")
